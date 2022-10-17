fa3d2059f9a5cb3c0becc750268e4c18984c7549
sielavic <63396872+sielavic@users.noreply.github.com>
2022-10-17 14:39:47 +0300

    Update comment.js

--- comment.js
+++ comment.js
@@ -122,3 +122,68 @@ function clickSubmit(news_id){
                               }
                         })
                   }
+
+
+   //Обработка ответов на комментарии
+                  function clickSubmitReply(answer_id){
+                        $("#comment-reply_"+answer_id).ajaxSubmit({
+                              type: "POST",
+                              url: "/main/send_reply_comment",
+                              success: function(data){
+                                    var data = JSON.parse(data);
+                                    if(data.status == "success") {
+
+                                          $(".reply-comment-form").clearForm();
+                                          $(".reply-comment-form").hide();
+
+
+
+
+                                          var reply_comment = $( '<div class="comments-block author-info reply-comment"  id="post_comment_'+data.result.id+'">\n' +
+                                          '<div class="row">\n' +
+                                          '<div class="col-lg-1 col-xs-2 text-center">\n' +
+                                          '<a href="/personal/profile/'+data.result.user_id+'" class="img-avatar">\n' +
+                                          '<img class="timeline-comment-ava" src="'+data.result.src+'">\n' +
+                                          '<i class="online-avatar-mob"></i>\n' +
+                                          '</a>\n' +
+                                          '</div>\n' +
+                                          '<div class="col-lg-3 col-xs-6">\n' +
+                                          '<p class="comment-name">'+ data.result.fio+'</p>\n' +
+                                          '<p class="comment-date" datetime="'+data.result.created+'">'+data.result.created+'</p>\n' +
+                                          '</div>\n' +
+                                          '<div class="col-lg-8 col-xs-12">\n' +
+                                          '<p>'+data.result.message+'</p>\n' +
+                                          '<a href="javascript:void(0);" class="pull-right btn_reply_comment"\n' +
+                                          'onclick="showNewsReply('+data.result.id+','+data.result.news_id+')"\n' +
+                                          'data-news-id="'+data.result.news_id+'"\n' +
+                                          'data-comment-id="'+data.result.id+'">Ответить</a>\n' +
+                                          '</div>\n' +
+                                          '</div>\n' +
+
+                                          '<div id="reply_form_'+data.result.id+'" style="display: none">\n' +
+                                          '<form action="/main/send_reply_comment"  id="comment-reply_'+data.result.id+'" class="reply-comment-form validate-form " role="form" method="POST">\n' +
+                                          '<input type="hidden" name="news_id" value="'+data.result.news_id+'">\n' +
+                                          '<input type="hidden" name="parent_id" value="'+data.result.id+'">\n' +
+                                          '<input type="hidden" name="user_id" value="'+data.result.user_id+'">\n' +
+                                          '<fieldset>\n' +
+                                          '<div class="input-row form-group padding-submit padding-bottom">\n' +
+                                          '<textarea  class="form-control input-field textarea textarea-comment" cols="30" rows="10" name="message" placeholder="Комментарий *"></textarea>\n' +
+                                          '<button class="fa fa-paper-plane-o reply-comment-submit pull-right" onclick="clickSubmitReply('+data.result.id+');return false;" type="submit"></button>\n' +
+                                          '</div>\n' +
+                                          '</fieldset>\n' +
+                                          '</form>\n' +
+                                          '</div>'
+                                          );
+
+
+                                          $('#post_comment_'+data.result.parent_id).append(reply_comment);
+
+
+
+                                          console.log(data);
+                                    }
+                              }
+                        })
+                  }
+
+
